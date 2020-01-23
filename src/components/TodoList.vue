1ba7198b8309501d796439c9c1cbbebeb15d63fc
moony01 <mun01180@gmail.com>
2020-01-23 19:11:36 +0900

    ing...

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,64 @@
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0;
+    margin-top: 0;
+    text-align: left;
+  }
+  li {
+    display: flex; /* 비율 기준의 레이아웃 방식 */
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+
+</style>
+
 <template>
-  <div class="">list</div>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems">
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type="button" @click="removeTodo">
+          <i class="far fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  }
 }
+// methods: {
+//   removeTodo: function(event){
+//     console.log("test");
+//   }
+// }
 </script>
-
-<style>
-</style>
